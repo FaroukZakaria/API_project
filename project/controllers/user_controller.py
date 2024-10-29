f24b3d37f66c8881a891a7d0d898f5739dfdb23c
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-10-30 00:36:47 +0300

    moved the token related function to its respective folder, removed unnecessary imports and removed unused function

--- project/controllers/user_controller.py
+++ project/controllers/user_controller.py
@@ -1,21 +1,6 @@
-import jwt
 from bson import ObjectId
-from flask_jwt_extended import decode_token
 from project.db import *
 from project.models.user_model import User
-
-def verify_token(token):
-    try:
-        return decode_token(token)
-    except jwt.ExpiredSignatureError:
-        #print("Token has expired.")
-        return None
-    except jwt.InvalidTokenError:
-        #print("Invalid token.")
-        return None
-    except Exception as e:
-        #print(f"An unexpected error occurred: {e}")
-        return None
 
 # Function to insert a user
 def create_user(user_data: User):
@@ -41,16 +26,3 @@ def get_user_by_id(user_id):
 def delete_user(user_id):
     result = users_collection.delete_one({"_id": ObjectId(user_id)})
     return result
-
-def refresh_token(token):
-    try:
-        return decode_token(token)
-    except jwt.ExpiredSignatureError:
-        #print("Token has expired.")
-        return None
-    except jwt.InvalidTokenError:
-        #print("Invalid token.")
-        return None
-    except Exception as e:
-        #print(f"An unexpected error occurred: {e}")
-        return None
