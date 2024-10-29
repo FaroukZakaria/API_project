c77e70dd50a84f1e01925c8bf7bf3465f0794d85
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-10-29 20:43:34 +0300

    database information file (mongodb)

---
+++ project/db.py
@@ -0,0 +1,10 @@
+import os
+from pymongo import MongoClient
+
+mongo_user = os.getenv('MONGODB_USERNAME')
+mongo_password = os.getenv('MONGODB_PASSWORD')
+mongo_uri = f"mongodb+srv://{mongo_user}:{mongo_password}@firstserver.z0ezt.mongodb.net/api-project?retryWrites=true&w=majority&appName=FirstServer"
+client = MongoClient(mongo_uri)
+db = client['api-project']
+users_collection = db['users']
+organizations_collection = db['organizations']
