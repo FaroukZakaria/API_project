847fdca46d70a66dac24de8d38705c7b4b96c011
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-10-30 00:38:06 +0300

    Removed unnecessary imports and added needed ones. moved token related routes to its respective file

--- project/routes/user_routes.py
+++ project/routes/user_routes.py
@@ -1,8 +1,9 @@
 import datetime, bcrypt
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
+from flask_jwt_extended import create_access_token, create_refresh_token
 from project.db import *
 from flask import request, jsonify, Blueprint, current_app
 from project.controllers.user_controller import *
+from project.controllers.token_controller import *
 
 user_routes = Blueprint('user_routes', __name__)
 
@@ -39,17 +40,3 @@ def login():
         refresh_token = create_refresh_token(str(user['_id']), expires_delta=datetime.timedelta(days=7))
         return jsonify({'access_token': access_token, 'refresh_token': refresh_token}), 200
     return jsonify({'message': 'Invalid credentials'}), 401
-
-@user_routes.route('/refresh-token', methods=['POST'])
-def refresh_token():
-    token = request.get_json().get('refresh_token')
-    if not token:
-        return jsonify({'message': 'Refresh token required'}), 401
-
-    decoded_token = verify_token(token)
-    if decoded_token:
-        if decoded_token['type'] != 'refresh':
-            return jsonify({'message': 'Not a refresh token'}), 401
-        new_access_token = create_access_token(decoded_token['sub'])
-        return jsonify({'message': 'Token refreshed', 'access_token': new_access_token, 'refresh_token': token}), 200
-    return jsonify({'message': 'Invalid or expired refresh token'}), 401
