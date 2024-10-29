3b0f29c054eee788b087bc0b42393c9f9af7d23a
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-10-30 00:49:21 +0300

    added a welcome at home

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from project import app
 
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
     print(f"Starting server: http://localhost:8080")
     app.run(host='localhost', port=8080, debug=True)
